7a8a5fe59ce1752b05b6edf06f4f0b76d3fd827b
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-14 00:28:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 class Student
 
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end
+
+  
 end
